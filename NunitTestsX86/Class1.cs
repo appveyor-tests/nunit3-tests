9d40f53957577b31625a703cd31dd7a9ffd5af41
Feodor Fitsner <feodor@appveyor.com>
2015-12-02 13:06:30 -0800

    Added x86 version of assembly

---
+++ NunitTestsX86/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTestsX86
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        [Category("A")]
+        public void TestA()
+        {
+            // appSettings
+            var someValue = "Hello, config!";
+            Assert.AreEqual("Hello, config!", someValue);
+        }
+    }
+}
